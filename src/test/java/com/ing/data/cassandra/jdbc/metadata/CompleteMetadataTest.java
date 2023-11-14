69ff689e9ed181fd08433f8fa8f187ed1f40429b
Sualeh Fatehi <sualeh@hotmail.com>
2023-11-14 13:29:28 -0500

    Fix Checkstyle issues

--- src/test/java/com/ing/data/cassandra/jdbc/metadata/CompleteMetadataTest.java
+++ src/test/java/com/ing/data/cassandra/jdbc/metadata/CompleteMetadataTest.java
@@ -28,68 +28,83 @@ import us.fatehi.utility.datasource.DatabaseConnectionSource;
 import us.fatehi.utility.datasource.DatabaseConnectionSources;
 import us.fatehi.utility.datasource.MultiUseUserCredentials;
 
+/****
+	* Runs extensive database metadata tests.
+****/
 @Testcontainers
 public class CompleteMetadataTest {
 
-  private final DockerImageName imageName = DockerImageName.parse("cassandra");
+    private final DockerImageName imageName = DockerImageName.parse("cassandra");
 
-  @Container
-  private final CassandraContainer<?> dbContainer =
-      new CassandraContainer<>(imageName.withTag("5.0")).withInitScript("initEmbeddedDse.cql");
+    @Container
+    private final CassandraContainer<?> dbContainer =
+            new CassandraContainer<>(imageName.withTag("5.0")).withInitScript("initEmbeddedDse.cql");
 
-  private DatabaseConnectionSource dbConnectionSource;
+    private DatabaseConnectionSource dbConnectionSource;
 
-  @BeforeEach
-  public void createDatabase() {
+    /****
+        * Sets up a data source to connect to the Cassandra database.
+        *
+    ****/
+    @BeforeEach
+    public void createDatabase() {
 
-    if (!dbContainer.isRunning()) {
-      fail("Testcontainer for database is not available");
+        if (!dbContainer.isRunning()) {
+            fail("Testcontainer for database is not available");
+        }
+
+        final InetSocketAddress contactPoint = dbContainer.getContactPoint();
+        final String host = contactPoint.getHostName();
+        final int port = contactPoint.getPort();
+        final String keyspace = "test_keyspace_vect";
+        final String localDatacenter = dbContainer.getLocalDatacenter();
+        final String connectionUrl = String.format("jdbc:cassandra://%s:%d/%s?localdatacenter=%s", host,
+                port, keyspace, localDatacenter);
+        System.out.printf("url=%s%n", connectionUrl);
+        createDataSource(connectionUrl);
     }
 
-    final InetSocketAddress contactPoint = dbContainer.getContactPoint();
-    final String host = contactPoint.getHostName();
-    final int port = contactPoint.getPort();
-    final String keyspace = "test_keyspace_vect";
-    final String localDatacenter = dbContainer.getLocalDatacenter();
-    final String connectionUrl = String.format("jdbc:cassandra://%s:%d/%s?localdatacenter=%s", host,
-        port, keyspace, localDatacenter);
-    System.out.printf("url=%s%n", connectionUrl);
-    createDataSource(connectionUrl);
-  }
+    /****
+        * Uses SchemaCrawler to obtain database metadata in "maximum" mode. 
+        * Compares actual results to an expected file with results. The test will
+        * produce a new expected results file if the actual and expected results
+        * do not match.
+		*
+		* @throws Exception on an error when running the test
+    ****/
+    @Test
+    public void givenDatabase_whenCompleteMetadataExtracted_shouldMatchExpectedOutput()
+            throws Exception {
 
-  @Test
-  public void givenDatabase_whenCompleteMetadataExtracted_shouldMatchExpectedOutput()
-      throws Exception {
+        final LimitOptionsBuilder limitOptionsBuilder =
+                LimitOptionsBuilder.builder().includeSchemas(Pattern.compile("test_keyspace_vect"));
+        final SchemaInfoLevelBuilder schemaInfoLevelBuilder =
+                SchemaInfoLevelBuilder.builder().withInfoLevel(InfoLevel.maximum);
+        final LoadOptionsBuilder loadOptionsBuilder =
+                LoadOptionsBuilder.builder().withSchemaInfoLevelBuilder(schemaInfoLevelBuilder);
+        final SchemaCrawlerOptions schemaCrawlerOptions = SchemaCrawlerOptionsBuilder
+                .newSchemaCrawlerOptions().withLimitOptions(limitOptionsBuilder.toOptions())
+                .withLoadOptions(loadOptionsBuilder.toOptions());
+        final SchemaTextOptionsBuilder textOptionsBuilder = SchemaTextOptionsBuilder.builder();
+        textOptionsBuilder.showDatabaseInfo().showJdbcDriverInfo();
+        final SchemaTextOptions textOptions = textOptionsBuilder.toOptions();
 
-    final LimitOptionsBuilder limitOptionsBuilder =
-        LimitOptionsBuilder.builder().includeSchemas(Pattern.compile("test_keyspace_vect"));
-    final SchemaInfoLevelBuilder schemaInfoLevelBuilder =
-        SchemaInfoLevelBuilder.builder().withInfoLevel(InfoLevel.maximum);
-    final LoadOptionsBuilder loadOptionsBuilder =
-        LoadOptionsBuilder.builder().withSchemaInfoLevelBuilder(schemaInfoLevelBuilder);
-    final SchemaCrawlerOptions schemaCrawlerOptions = SchemaCrawlerOptionsBuilder
-        .newSchemaCrawlerOptions().withLimitOptions(limitOptionsBuilder.toOptions())
-        .withLoadOptions(loadOptionsBuilder.toOptions());
-    final SchemaTextOptionsBuilder textOptionsBuilder = SchemaTextOptionsBuilder.builder();
-    textOptionsBuilder.showDatabaseInfo().showJdbcDriverInfo();
-    final SchemaTextOptions textOptions = textOptionsBuilder.toOptions();
+        final SchemaCrawlerExecutable executable = new SchemaCrawlerExecutable("details");
+        executable.setSchemaCrawlerOptions(schemaCrawlerOptions);
+        executable.setAdditionalConfiguration(SchemaTextOptionsBuilder.builder(textOptions).toConfig());
 
-    final SchemaCrawlerExecutable executable = new SchemaCrawlerExecutable("details");
-    executable.setSchemaCrawlerOptions(schemaCrawlerOptions);
-    executable.setAdditionalConfiguration(SchemaTextOptionsBuilder.builder(textOptions).toConfig());
+        final String expectedResource = "expected_metadata_output.txt";
+        assertThat(outputOf(executableExecution(getDataSource(), executable)),
+                hasSameContentAs(classpathResource(expectedResource)));
+    }
 
-    final String expectedResource = "expected_metadata_output.txt";
-    assertThat(outputOf(executableExecution(getDataSource(), executable)),
-        hasSameContentAs(classpathResource(expectedResource)));
-  }
+    private void createDataSource(final String connectionUrl) {
+        dbConnectionSource = DatabaseConnectionSources.newDatabaseConnectionSource(connectionUrl,
+                new MultiUseUserCredentials(dbContainer.getUsername(), dbContainer.getPassword()));
 
-  private void createDataSource(final String connectionUrl) {
-    dbConnectionSource = DatabaseConnectionSources.newDatabaseConnectionSource(connectionUrl,
-        new MultiUseUserCredentials(dbContainer.getUsername(), dbContainer.getPassword()));
+    }
 
-  }
-
-  private DatabaseConnectionSource getDataSource() {
-    return dbConnectionSource;
-  }
+    private DatabaseConnectionSource getDataSource() {
+        return dbConnectionSource;
+    }
 }
